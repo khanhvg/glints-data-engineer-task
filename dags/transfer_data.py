ac8121a4b0de138a3df20658f63304f5253194bf
Khánh Vũ <khanhvu@Khanhs-MacBook-Air.local>
2022-05-16 00:14:27 +0700

    Change to use dag decorator

--- dags/transfer_data.py
+++ dags/transfer_data.py
@@ -2,39 +2,32 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
+from airflow.decorators import dag
 
 
-# Define default_args
-default_args = {
-    "owner": "GiaKhanh",
-    "start_date": datetime(2022, 5, 13),
-    "schedule_interval": "@once",
-    "catch_up": True
-}
+def transfer_data():
+    # Create sql command to select all data from source database
+    # Then define a source hook using PostgresHook
+    # Then use cursor to execute sql query and fetch all data return
+    sql_select = "SELECT product_id,product_name,price FROM product"
+    source_hook = PostgresHook(postgres_conn_id="source_postgres")
+    source_conn = source_hook.get_conn()
+    source_cursor = source_conn.cursor()
+    source_cursor.execute(sql_select)
+    rows = source_cursor.fetchall()
+
+    # Create target hook with PostgresHook
+    # Then using insert_rows function to insert into target database
+    target_hook = PostgresHook(postgres_conn_id="target_postgres")
+    target_hook.insert_rows(table='product', rows=rows)
 
 
-def get_data_from_source():
-  # Create sql command to select all data from source database
-  # Then define a source hook using PostgresHook
-  # Then use cursor to execute sql query and fetch all data return
-  sql_select = "SELECT product_id,product_name,price FROM product"
-  source_hook = PostgresHook(postgres_conn_id="source_postgres")
-  source_conn = source_hook.get_conn()
-  source_cursor = source_conn.cursor()
-  source_cursor.execute(sql_select)
-  rows = source_cursor.fetchall()
+@dag(schedule_interval='@once', start_date=datetime(2022, 5, 13), catchup=False)
+def transfer_data_postgres():
 
-  # Create target hook with PostgresHook
-  # Then using insert_rows function to insert into target database
-  target_hook = PostgresHook(postgres_conn_id="target_postgres")
-  target_hook.insert_rows(table='product', rows=rows)
+    PythonOperator(
+      task_id = 'transfer_data',
+      python_callable = transfer_data
+    )
 
-
-with DAG("transfer_data_postgres", default_args=default_args) as dag:
-  
-  t1 = PythonOperator(
-      task_id='get_data_from_source',
-      python_callable=get_data_from_source
-  )
-
-  t1
+dag = transfer_data_postgres()
